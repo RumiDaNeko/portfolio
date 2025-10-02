3f99a0c12bec95c111c523f687c6664bef45c834
Harumiii._. <77107580+RumiDaNeko@users.noreply.github.com>
2025-10-03 04:33:50 +0700

    Update chat.ts

--- src/pages/api/chat.ts
+++ src/pages/api/chat.ts
@@ -1,22 +1,20 @@
 import type { APIRoute } from 'astro';
-import { GoogleGenAI } from "@google/genai";
+import OpenAI from "openai";
 
-const gemini = new GoogleGenAI({apiKey: import.meta.env.AI_API_KEY});
+const gemini = new OpenAI({
+    apiKey: import.meta.env.AI_API_KEY,
+    baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/"
+});
 
 export const POST: APIRoute = async ({ request }) => {
   try {
     const body = await request.json();
 
-    const completion = await gemini.models.generateContent({
+    const completion = await gemini.chat.completions.create({
     model: "gemini-2.5-flash",
-    contents: body.messages,
-    config: {
-      thinkingConfig: {
-        thinkingBudget: 10, // Disables thinking
-      },
+    messages: body.messages,
       temperature: 0.7,
-      maxOutputTokens: 500,
-    }
+      max_tokens: 500,
   });
 
     return new Response(
